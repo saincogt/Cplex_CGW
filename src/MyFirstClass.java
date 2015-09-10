1297575de3b0c7817124e923bcfc70bd6204c36b
Sancho <Sancho@138_80_101_212.cdu.edu.au>
2015-09-10 15:33:31 +0930

    Test

---
+++ src/MyFirstClass.java
@@ -0,0 +1,20 @@
+import ilog.concert.*;
+import ilog.cplex.*;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void model1() {
+		try {
+			
+		}
+		catch (IloException exc) {
+			
+		}
+	}
+
+}
