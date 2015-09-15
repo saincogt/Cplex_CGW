d774b952ce31feb14cb701b8ba21d4dbdc8920a1
Sancho <Sancho@138_80_97_72.cdu.edu.au>
2015-09-15 16:51:01 +0930

    0915v3 Edited and deleted several files as Peter asked.

--- src/CplexCGWMain.java
+++ src/CplexCGWMain.java
@@ -1,43 +1,41 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.*;
-
+import ilog.concert.*;
+import ilog.cplex.*;
 
 public class CplexCGWMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		myTest();
+		ApplyCplex();
 	}
 	
-	public static void myTest() {
-		int[] vector = {1, 233333, 333, 43333, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, };
-		int bit = 2;
-		int RSV = 5;
-		int XBM = 31;
+	public static void ApplyCplex() {
+		try {
+			
+			IloCplex cplex = new IloCplex();
+			
+			// Variables
+			IloNumVar Xij = cplex.numVar(0.0, 1.0, "Xij");
+			
+			// Expressions
+			IloLinearNumExpr objective = cplex.linearNumExpr();
+			objective.addTerm(0.12, Xij);
+			
+			// Define objectives
+			cplex.addMinimize(objective);
+			
+			
+			// Define constraints
+			cplex.addGe(cplex.addGe(Xij, Xij), Xij);
+			
+			// Solve
+			cplex.solve();
+						
+		}
+		catch (IloException exc) {
+			exc.getStackTrace();
+		}
 		
-		Stack<String> s = new Stack<String>();
-		s.push("aaa");
-		s.push("bbbb");
-		System.out.println(s.pop());
-		System.out.println(s.pop());
-		Set<String> set = new HashSet<String>();
-		
-		set.add("Hello");
-		set.add("Yo");
-		System.out.println(set.contains("Yes"));
-		set.remove("Yo");
-		set.add("Yes");
-		System.out.println(set.contains("Yo"));
-		System.out.println(set.contains("Yes"));
 
-		
-		
-		System.out.println((2)&1<<1);
-		System.out.println(vector[bit]);
-		System.out.println(333 >> RSV);
-		System.out.println(vector[(bit) >> RSV]);
-		System.out.println("This is my test:");
-		System.out.println(vector[(bit) >> RSV] & 1 << ((bit) & XBM));
+
 	}
 }
