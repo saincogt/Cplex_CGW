26e16418e47be42d14f43e41d007d9dab8e314cc
saincogt <saincogt@gmail.com>
2015-09-16 15:53:03 +0930

    0916v2

--- src/CplexCGWMain.java
+++ src/CplexCGWMain.java
@@ -15,21 +15,23 @@ public class CplexCGWMain {
 			
 			// Variables
 			IloNumVar Xij = cplex.numVar(0.0, 1.0, "Xij");
+			IloNumVar Xjk = cplex.numVar(0.1, 2.0, "Xjk");
 			
 			// Expressions
 			IloLinearNumExpr objective = cplex.linearNumExpr();
 			objective.addTerm(0.12, Xij);
+			objective.addTerm(0.11, Xjk);
+			
 			
 			// Define objectives
 			cplex.addMinimize(objective);
 			
 			
 			// Define constraints
-			cplex.addGe(cplex.addGe(Xij, Xij), Xij);
+			cplex.addGe(cplex.addGe(Xij, Xij), Xjk);
 			
 			// Solve
 			cplex.solve();
-			
 						
 		}
 		catch (IloException exc) {
